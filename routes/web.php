b7de976c35061c022db68c1a3001464f628a1552
dani saputra <mochamad.saputra@ist.id>
2024-12-17 09:31:34 +0700

    start hexohm

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//credentials admin login hexohm-blog
+// email: hexohmsolo@example.com
+// password: hexsolo111
